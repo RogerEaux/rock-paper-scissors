02b69c49b67d2f100fad684f8a0c80c8fa02ea0a
Roberto Meré <121988408+RogerEaux@users.noreply.github.com>
2023-01-19 01:17:57 -0600

    Add function simulate a round of the game

--- scripts/game.js
+++ scripts/game.js
@@ -2,12 +2,12 @@ function getComputerChoice(){
     let computerChoice
     let randomNumber
     randomNumber = Math.floor(Math.random()*3) + 1
-    if (randomNumber === 1){
-        computerChoice = 'Rock'
-    }else if (randomNumber === 2){
-        computerChoice = 'Paper'
+    if(randomNumber === 1){
+        computerChoice = 'rock'
+    }else if(randomNumber === 2){
+        computerChoice = 'paper'
     }else{
-        computerChoice = 'Scissors'
+        computerChoice = 'scissors'
     }
 
     return computerChoice
@@ -22,4 +22,31 @@ function getPlayerChoice(){
     return playerChoice
 }
 
-console.log(getPlayerChoice())
+function playRound(){
+    const playerChoice = getPlayerChoice()
+    const computerChoice = getComputerChoice()
+    let result
+    if(playerChoice === computerChoice){
+        result = 'It\s a tie'
+    }else if(playerChoice === 'rock'){
+        if(computerChoice === 'paper'){
+            result = 'You lose! Rock beats Paper'
+        }else{
+            result = 'You win! Rock beats Scissors'
+        }
+    }else if(playerChoice === 'paper'){
+        if(computerChoice === 'rock'){
+            result = 'You win! Rock beats Paper'
+        }else{
+            result = 'You lose! Scissors beats Paper'
+        }
+    }else if(playerChoice === 'scissors'){
+        if(computerChoice === 'paper'){
+            result = 'You win! Scissors beats Paper'
+        }else{
+            result = 'You lose! Rock beats Scissors'
+        }
+    }
+
+    return result
+}
