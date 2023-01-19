a5752c89f93166eda2bffef9ed193bfea16eea41
Roberto Meré <121988408+RogerEaux@users.noreply.github.com>
2023-01-19 00:49:00 -0600

    Add function to input players choice

--- scripts/game.js
+++ scripts/game.js
@@ -1,6 +1,6 @@
 function getComputerChoice(){  
-    let computerChoice = ''
-    let randomNumber = 0
+    let computerChoice
+    let randomNumber
     randomNumber = Math.floor(Math.random()*3) + 1
     if (randomNumber === 1){
         computerChoice = 'Rock'
@@ -12,3 +12,14 @@ function getComputerChoice(){
 
     return computerChoice
 }
+
+function getPlayerChoice(){
+    let playerChoice
+    while(playerChoice !== 'rock' && playerChoice !== 'paper' && playerChoice !== 'scissors'){
+        playerChoice = prompt('Rock, paper, scissors, shoot!').toLowerCase()
+    }
+
+    return playerChoice
+}
+
+console.log(getPlayerChoice())
