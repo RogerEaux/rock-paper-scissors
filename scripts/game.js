de0db86d5a123ad3152e1dbf01c8dfc435835a62
Roberto Meré <121988408+RogerEaux@users.noreply.github.com>
2023-01-21 16:56:32 -0600

    Add buttons for user to make their choice

--- scripts/game.js
+++ scripts/game.js
@@ -1,64 +1,67 @@
 function getComputerChoice(){  
-    let computerChoice
-    let randomNumber
-    randomNumber = Math.floor(Math.random()*3) + 1
+    let computerChoice;
+    let randomNumber;
+    randomNumber = Math.floor(Math.random()*3) + 1;
     if(randomNumber === 1){
-        computerChoice = 'rock'
+        computerChoice = 'Rock';
     }else if(randomNumber === 2){
-        computerChoice = 'paper'
+        computerChoice = 'Paper';
     }else{
-        computerChoice = 'scissors'
+        computerChoice = 'Scissors';
     }
 
     return computerChoice
 }
 
-function getPlayerChoice(){
-    let playerChoice
-    while(playerChoice !== 'rock' && playerChoice !== 'paper' && playerChoice !== 'scissors'){
-        playerChoice = prompt('Rock, paper, scissors, shoot!').toLowerCase()
-    }
-
-    return playerChoice
-}
-
-function playRound(){
-    const playerChoice = getPlayerChoice()
-    const computerChoice = getComputerChoice()
-    let result
+function playRound(playerChoice){
+    const computerChoice = getComputerChoice();
+    let result;
     if(playerChoice === computerChoice){
         result = 'It\s a tie'
-    }else if(playerChoice === 'rock' && computerChoice === 'paper' || playerChoice === 'paper' && computerChoice === 'scissors' || playerChoice === 'scissors' && computerChoice === 'rock' ){
-        result = `You lose, ${computerChoice} beats ${playerChoice}`
-    }else if(playerChoice === 'rock' && computerChoice === 'scissors' || playerChoice === 'paper' && computerChoice === 'rock' || playerChoice === 'scissors' && computerChoice === 'paper' ){
-        result = `You win, ${playerChoice} beats ${computerChoice}`
+    }else if(playerChoice === 'Rock' && computerChoice === 'Paper' || playerChoice === 'Paper' && computerChoice === 'Scissors' || playerChoice === 'Scissors' && computerChoice === 'Rock' ){
+        result = `You lose, ${computerChoice} beats ${playerChoice}`;
+    }else if(playerChoice === 'Rock' && computerChoice === 'Scissors' || playerChoice === 'Paper' && computerChoice === 'Rock' || playerChoice === 'Scissors' && computerChoice === 'Paper' ){
+        result = `You win, ${playerChoice} beats ${computerChoice}`;
     }
 
-    return result
+    console.log(result);
+    return result;
 }
 
 function game(){
-    let result
-    let playerScore = 0
-    let computerScore = 0
-    let winner
+    const buttons = document.querySelectorAll('button');
+    buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        const playerChoice = button.textContent;
+        playRound(playerChoice);
+    });
+});
+}
+
+game();
+
+/*function game(){
+    let result;
+    let playerScore = 0;
+    let computerScore = 0;
+    let winner;
     for(let i = 0; i < 5; i++){
-        result = playRound()
-        console.log(result)
+        result = playRound();
+        console.log(result);
         if(result.slice(4,5) === 'w'){
-            playerScore++
+            playerScore++;
         }else if(result.slice(4,5) === 'l'){
-            computerScore++
+            computerScore++;
         }
     }
     if(playerScore > computerScore){
-        winner = `Player wins ${playerScore} to ${computerScore}` 
+        winner = `Player wins ${playerScore} to ${computerScore}`;
     }else if(computerScore > playerScore){
-        winner = `Computer wins ${computerScore} to ${playerScore}` 
+        winner = `Computer wins ${computerScore} to ${playerScore}`; 
     }else{
-        winner = 'Neither machine nor man'
+        winner = 'Neither machine nor man';
     }
-    console.log(winner)
-}
+    console.log(winner);
+}*/
 
-game()
+
