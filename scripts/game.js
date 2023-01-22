f10499828f5e10a0b916f0c3282e3d78e67a9533
Roberto Meré <121988408+RogerEaux@users.noreply.github.com>
2023-01-21 21:04:03 -0600

    Restructure functions to allow to play a round when an image is clicked

--- scripts/game.js
+++ scripts/game.js
@@ -1,50 +1,3 @@
-function getComputerChoice(){  
-    let computerChoice;
-    let randomNumber;
-    randomNumber = Math.floor(Math.random()*3) + 1;
-    if(randomNumber === 1){
-        computerChoice = 'Rock';
-    }else if(randomNumber === 2){
-        computerChoice = 'Paper';
-    }else{
-        computerChoice = 'Scissors';
-    }
-
-    return computerChoice
-}
-
-function playRound(playerChoice){
-    const computerChoice = getComputerChoice();
-    let result;
-    if(playerChoice === computerChoice){
-        result = 'It\s a tie'
-    }else if(playerChoice === 'Rock' && computerChoice === 'Paper' || playerChoice === 'Paper' && computerChoice === 'Scissors' || playerChoice === 'Scissors' && computerChoice === 'Rock' ){
-        result = `You lose, ${computerChoice} beats ${playerChoice}`;
-    }else if(playerChoice === 'Rock' && computerChoice === 'Scissors' || playerChoice === 'Paper' && computerChoice === 'Rock' || playerChoice === 'Scissors' && computerChoice === 'Paper' ){
-        result = `You win, ${playerChoice} beats ${computerChoice}`;
-    }
-
-    console.log(result);
-    return result;
-}
-
-function getPlayerChoice(){
-    const images = document.querySelectorAll('img');
-    images.forEach((image) => {
-        image.addEventListener('click', () => {
-            let playerChoice;
-            if(image.id === 'rock'){
-                playerChoice = 'Rock';
-            }else if(image.id === 'paper'){
-                playerChoice = 'Paper';
-            }else{
-                playerChoice = 'Scissors';
-            }
-        playRound(playerChoice);
-    });
-});
-}
-
 function changeHeader(){
     const content = document.querySelector('#content');
     content.removeChild(start);
@@ -94,11 +47,45 @@ function startGame(){
     addImages();
 }
 
+function getComputerChoice(){  
+    let computerChoice;
+    let randomNumber;
+    randomNumber = Math.floor(Math.random()*3) + 1;
+    if(randomNumber === 1){
+        computerChoice = 'rock';
+    }else if(randomNumber === 2){
+        computerChoice = 'paper';
+    }else{
+        computerChoice = 'scissors';
+    }
+
+    return computerChoice
+}
+
+function playRound(){
+    const playerChoice = this.id
+    const computerChoice = getComputerChoice();
+    console.log(computerChoice);
+    let result;
+    if(playerChoice === computerChoice){
+        result = 'It\s a tie'
+    }else if(playerChoice === 'rock' && computerChoice === 'paper' || playerChoice === 'paper' && computerChoice === 'scissors' || playerChoice === 'scissors' && computerChoice === 'rock' ){
+        result = `You lose, ${computerChoice} beats ${playerChoice}`;
+    }else if(playerChoice === 'rock' && computerChoice === 'scissors' || playerChoice === 'paper' && computerChoice === 'rock' || playerChoice === 'scissors' && computerChoice === 'paper' ){
+        result = `You win, ${playerChoice} beats ${computerChoice}`;
+    }
+
+    console.log(result);
+    return result;
+}
+
 function game(){
     const start = document.querySelector('#start');
     start.addEventListener('click', startGame);
-    getPlayerChoice();
-
+    const images = document.querySelectorAll('img');
+    images.forEach((image) =>{
+        image.addEventListener('click', playRound);
+    });
 }
 
 game();
